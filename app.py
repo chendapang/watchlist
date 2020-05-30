b91310c8379d84832f1776cd5ce30300c5835f6b
chentao <8203179@qq.com>
2020-05-31 07:35:45 +0800

    '修改/test'

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
-from flask import Flask
+from flask import Flask, url_for
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return 'hello Flask'
+
+
+@app.route('/user/<name>')
+def user_page(name):
+    return f'User:{name}'
+
+
+@app.route('/test')
+def test_url_for():
+    print(url_for('hello_world'))
+    print(url_for('user_page', name='zhangsan'))
+    print(url_for('user_page', name='lisi'))
+    print(url_for('test_url_for'))
+    print(url_for('test_url_for', num=2))
+    return 'Test page'
 
 
 if __name__ == '__main__':
